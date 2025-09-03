20ba4958346825622232b833c72b7cb7570d72bc
pawel <pawel@werda.pl>
2025-09-03 17:07:20 +0200

    chore: bump version to 0.2.0 and update README

--- check_circular_import/__init__.py
+++ check_circular_import/__init__.py
@@ -1,6 +1,6 @@
 """Check Circular Import - A pre-commit hook for detecting circular imports."""
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from check_circular_import.detector import CircularImportDetector
 
